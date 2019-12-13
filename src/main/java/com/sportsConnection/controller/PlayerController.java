8808479a69a89c1ac5f86ca977914e3e53a812d1
Zach <new14008@byui.edu>
2019-12-12 22:39:58 -0800

    changed ports

--- src/main/java/com/sportsConnection/controller/PlayerController.java
+++ src/main/java/com/sportsConnection/controller/PlayerController.java
@@ -4,7 +4,11 @@ import com.sportsConnection.entity.Player;
 import com.sportsConnection.entity.Sport;
 import com.sportsConnection.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
--- src/main/java/com/sportsConnection/controller/SportController.java
+++ src/main/java/com/sportsConnection/controller/SportController.java
@@ -1,10 +1,13 @@
 package com.sportsConnection.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
 import com.sportsConnection.entity.Sport;
 import com.sportsConnection.service.SportService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Map;
